44562308a84435623ef4ac22b623bbbfcde933fe
Elio D'antoni <elio.dantoni@gmail.com>
2016-01-20 10:18:48 +0100

    Initial Commit

---
+++ Runny/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Runny
+//
+//  Created by letterio d'antoni on 20/01/16.
+//  Copyright © 2016 letterio d'antoni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
